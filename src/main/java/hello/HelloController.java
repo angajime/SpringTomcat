8cfef1b1995048ec45e1bb5a744b76b3634cf24a
Antonio <antoniogarciajimenez91@gmail.com>
2014-10-27 11:15:07 +0100

    Generando nombres

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,26 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+
+        return "Bienvenido a Talericsson.io<br>" +
+                "Para empezar a enviar datos acceda a la siguiente dirección:<br>" +
+                "<a href='http://localhost:8080/send/as/beasty-beast'>" +
+                "http://localhost:8080/send/as/beasty-beast" +
+                "</a>";
+    }
+
+    @RequestMapping("/send/as/{id}")
+    public String del(@PathVariable("id") String id) {
+        return "Hola " +
+                id +"! :) Estamos recibiendo tus datos!!";
     }
 
 }
